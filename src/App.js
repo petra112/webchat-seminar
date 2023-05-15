f882cb706d4aecd11357e9b4b83380c99020b314
Petra Vahov <vlahovpetra@gmail.com>
2023-05-15 18:54:39 +0200

    finished seminar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [drone, setDrone] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [user, setUser] = useState();
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (user) {
+      const drone = new window.Scaledrone("hBGJheR5p6kvOoXW", {
+        data: user,
+      });
+      setDrone(drone);
+
+      const room = drone.subscribe("observable-room");
+
+      room.on("data", (message, user) => {
+        console.log(message);
+        setMessages((prevMessages) => [...prevMessages, { message, user }]);
+      });
+    }
+  }, [user]);
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (message) {
+      drone.publish({
+        room: "observable-room",
+        message,
+      });
+      setMessage("");
+    }
+  };
+  function randomColor() {
+    return "#" + Math.floor(Math.random() * 0xffffff).toString(16);
+  }
+
+  const loginUser = (e) => {
+    e.preventDefault();
+    setUser({
+      username: e.target.elements.username.value,
+      color: randomColor(),
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="main-wrapper">
+          {user ? (
+            <>
+              <h1>Chat</h1>
+              <div className="chat-container">
+                <div className="messages">
+                  {messages.map((msg, index) => (
+                    <div key={index}>
+                      <p>{msg.user.clientData.username}:</p>
+                      <span
+                        className="message"
+                        style={{
+                          backgroundColor: msg.user.clientData.color,
+                        }}
+                      >
+                        {msg.message}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+                <input
+                  type="text"
+                  value={message}
+                  onChange={handleMessageChange}
+                />
+                <button onClick={handleSendMessage}>Send</button>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="login">
+                <form className="login-form" onSubmit={loginUser}>
+                  <div>
+                    <p>Enter name</p>
+                    <input
+                      name="username"
+                      className="username"
+                      type="text"
+                    ></input>
+                  </div>
+                  <div>
+                    <button type="submit" className="button">
+                      Login
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
